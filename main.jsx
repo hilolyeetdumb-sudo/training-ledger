ba59fa3a9f9667924fef6e9ef59516036eb01842
hilolyeetdumb-sudo <hilolyeetdumb@gmail.com>
2026-08-19 00:12:27 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TrainingLedger from './TrainingLedger.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TrainingLedger />
-  </React.StrictMode>,
-)
