8cb8ca33b08bcc2f30c1680ae00f65b3cc0cb454
hilolyeetdumb-sudo <hilolyeetdumb@gmail.com>
2026-08-19 00:13:09 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TrainingLedger from './TrainingLedger.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TrainingLedger />
+  </React.StrictMode>,
+)
